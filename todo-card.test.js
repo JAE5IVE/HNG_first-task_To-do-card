96b172df860b3db188b37b7b6006273387c7211b
JAE5IVE <josephtuta20@gmail.com>
2026-04-12 20:06:15 +0100

    Rename todo-card.html to index.html for Netlify deployment

--- todo-card.test.js
+++ todo-card.test.js
@@ -9,7 +9,7 @@ import { fileURLToPath } from 'url'
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
-const html = fs.readFileSync(path.resolve(__dirname, 'todo-card.html'), 'utf8')
+const html = fs.readFileSync(path.resolve(__dirname, 'index.html'), 'utf8')
 
 describe('Todo Card', () => {
   let container
